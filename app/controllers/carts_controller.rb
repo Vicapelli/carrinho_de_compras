e2a7351687437456614a47a5750bf675457141fb
Victor Capelli <victor.capelli@vollsolutions.com.br>
2025-06-30 10:48:06 -0300

    remove concern

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,4 @@
 class CartsController < ApplicationController
-  include HandlesCartItems
   before_action :set_cart
 
   def show
@@ -37,6 +36,31 @@ class CartsController < ApplicationController
 
   def set_cart
     @cart = Cart.find_by(id: session[:cart_id] || item_params[:id])
+  end
+
+  def add_or_update_cart_item(product_id, quantity)
+    quantity = params[:quantity].to_i
+    cart_item = @cart.cart_items.find_or_initialize_by(product_id: product_id)
+    cart_item.quantity ||= 0
+    cart_item.quantity += quantity
+    cart_item.save!
+    cart_item
+  end
+
+  def cart_response
+    {
+      id: @cart.id,
+      products: @cart.cart_items.includes(:product).map do |item|
+        {
+          id: item.product.id,
+          name: item.product.name,
+          quantity: item.quantity,
+          unit_price: item.product.price.to_f,
+          total_price: (item.product.price * item.quantity).to_f
+        }
+      end,
+      total_price: @cart.total_price.to_f
+    }
   end
 
   def item_params
